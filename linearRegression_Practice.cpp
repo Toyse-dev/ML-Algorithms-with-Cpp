8ccc0da1f05cd553fbd35b125537c3b895dc51fd
Toyse-dev <olayonwatoyib05@gmail.com>
2026-09-17 14:40:46 +0100

    Implement prediction functions for linear regression
    Added functions for single and multiple predictions in linear regression.

--- linearRegression_Practice.cpp
+++ linearRegression_Practice.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <vector>
 
-int main () {
-	
-	double prediction(const std::vector<double>& weight,
+//First function: Predict one sample
+double prediction(const std::vector<double>& weight,
 	double bias,
-	const std::vector<double>* x) {
+	const std::vector<double>& x) {
 		double predict = bias;
 		
 		for (size_t j = 0; j < weight.size(); ++j) {
@@ -13,6 +12,23 @@ int main () {
 		}
 		return predict;
 	}
+	
+	// Predict many
+	std::vector<double> predictAll(const std::vector<std::vector<double>>& X,
+	double bias,
+	std::vector<double>& weight) {
+		std::vector<double> results;
+		for (const auto& row : X) {
+			double predict = bias;
+			for (size_t j = 0; j < weight.size(); ++j) {
+				predict += weight[j] * row[j];
+			}
+			results.push_back(predict);
+		}
+		return results;
+	}
+
+int main () {
 	
 	return 0;
 }
